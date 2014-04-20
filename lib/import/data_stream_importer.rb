bb02d5bfac03d898b2e3b63e2f96c5e2b15f1194
Jonathan E. Magen <yonkeltron@gmail.com>
2014-04-20 14:34:16 -0400

    very basic hash construction

--- lib/import/data_stream_importer.rb
+++ lib/import/data_stream_importer.rb
@@ -1,3 +1,29 @@
+require 'csv'
+
 class DataStreamImporter
+  include Contracts
   include ActiveModel::Model
+
+  attr_accessor :stream
+
+  Contract nil => ArrayOf[ArrayOf[Or[String,Num,nil]]]
+  def rows
+    @rows ||= CSV.parse(stream)
+    header
+    @rows
+  end
+
+  Contract nil => Maybe[ArrayOf[Symbol]]
+  def header
+    @header ||= @rows.shift.map(&:to_sym) if @rows
+  end
+
+  Contract nil => ArrayOf[HashOf[Symbol, Or[String,Num,nil]]]
+  def to_hashes
+    output = rows.map do |row|
+      Hash[header.zip(row)]
+    end
+
+    output
+  end
 end
--- spec/lib/import/data_stream_importer_spec.rb
+++ spec/lib/import/data_stream_importer_spec.rb
@@ -1,4 +1,23 @@
 require 'spec_helper'
 
 describe DataStreamImporter do
+  let(:path) { Rails.root.join('spec', 'data', 'test_import_data.csv').to_s }
+  let(:stream) { File.read(path) }
+  let(:data_stream_importer) { DataStreamImporter.new(stream: stream) }
+
+  describe '#header' do
+    context 'length' do
+      it do
+        data_stream_importer.rows; expect(data_stream_importer.header.length).to eq(117)
+      end
+    end
+  end
+
+  describe '#to_hashes' do
+    let(:output) { data_stream_importer.to_hashes }
+
+    context 'length' do
+      it { expect(output.length).to eq(89) }
+    end
+  end
 end
