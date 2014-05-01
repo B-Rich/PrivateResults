9779ab108bb477760f5d1ddec2122777ff2f4c4c
Jonathan E. Magen <yonkeltron@gmail.com>
2014-05-01 16:18:05 -0400

    spec optimization and profiling

--- spec/lib/import/insight/csv_importer_spec.rb
+++ spec/lib/import/insight/csv_importer_spec.rb
@@ -1,13 +1,20 @@
 require 'spec_helper'
 
 describe Insight::CsvImporter do
-  let(:path) { Rails.root.join('spec', 'data', 'test_import_data.csv').to_s }
-  let(:stream) { File.open(path, 'rb') }
-  let(:row_hash) { DataStreamImporter.new(stream: stream).to_hashes.first }
+  before(:all) do
+    @stream = StringIO.new(File.read(Rails.root.join('spec', 'data', 'test_import_data.csv').to_s))
+    @row_hashes = DataStreamImporter.new(stream: @stream).to_hashes
+  end
 
-  after(:each) { stream.close }
+  let(:stream) { @stream }
+
+  after(:each) { stream.rewind }
 
   let(:csv_importer) { Insight::CsvImporter.new(stream: stream) }
+
+  before(:each) do
+    csv_importer.stub(:row_hashes => @row_hashes)
+  end
 
   describe '#import!' do
     context 'new records' do
