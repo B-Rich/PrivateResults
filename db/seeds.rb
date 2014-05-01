19149ba555311de71bc2e35ba539625c3b311fa0
Jonathan E. Magen <yonkeltron@gmail.com>
2014-05-01 15:11:53 -0400

    spelling. d'oh.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-%W{Chlamydia Gonhorrea Syphilis HIV Trichomoniasis}.each do |infection_name|
+%W{Chlamydia Gonorrhea Syphilis HIV Trichomoniasis}.each do |infection_name|
   Rails.logger.info("Seeding #{Infection.model_name.human}: #{infection_name}")
   Infection.where(name: infection_name).first_or_create!
 end
