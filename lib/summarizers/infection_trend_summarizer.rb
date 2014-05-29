adcfbb0b1dd60ba8c8817d08c6d62249a1ccc276
Jonathan E. Magen <yonkeltron@gmail.com>
2014-05-29 11:39:52 -0400

    infection trend summarizer refactor

--- lib/summarizers/infection_trend_summarizer.rb
+++ lib/summarizers/infection_trend_summarizer.rb
@@ -27,13 +27,35 @@ class InfectionTrendSummarizer
   def infection_counts(infection)
     Rails.logger.info("Calculating trends for #{Infection.model_name.human} #{infection.name} from #{from} -- #{to}")
     (from.to_date..to.to_date).step(INTERVAL).each_with_object({}) do |date, hash|
-      range = (date..(date + (INTERVAL - 1).days).to_date)
+      range = infection_counts_range(date, INTERVAL)
 
-      hash[date] = infection.results
-                   .positive
-                   .joins(:visit)
-                   .where(Visit.arel_table[:visited_on].in range)
-                   .count
+      hash[date] = infection_count(infection, range)
     end
+  end
+
+  Contract Infection, Range => Num
+  # Computes {Infection} count
+  #
+  # @api private
+  # @param infection [Infection] the infection for which to calculate counts
+  # @param range [Range] the range for query
+  # @return [Fixnum] the computed count (gteq 0)
+  def infection_count(infection, range)
+    infection.results
+      .positive
+      .joins(:visit)
+      .where(Visit.arel_table[:visited_on].in range)
+      .count
+  end
+
+  Contract Date, Num => Range
+  # Generates Range for {Infection} count query
+  #
+  # @api private
+  # @param date [Date] the date from which to calculate the interval
+  # @param interval [Fixnum] the interval for calculation
+  # @return [Range] the calculated range
+  def infection_counts_range(date, interval)
+    date..(date + (INTERVAL - 1).days).to_date
   end
 end
