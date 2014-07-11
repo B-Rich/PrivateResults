bf495e17e32f403997d23d3a3e50e04bd6afd143
Kafu Chau <kchau@twilio.com>
2014-07-11 14:17:52 -0700

    Remove unnecessary initializer

--- config/initializers/twilio.rb
+++
@@ -1,8 +0,0 @@
-TWILIO_ACCT_SID = ENV['TWILIO_ACCT_SID']
-TWILIO_TOKEN    = ENV['TWILIO_TOKEN']
-
-if TWILIO_ACCT_SID.nil? or TWILIO_TOKEN.nil?
-  raise RuntimeError, "Invalid Twilio Account SID and/or API Token"
-end
-
-TWILIO = Twilio::REST::Client.new(TWILIO_ACCT_SID, TWILIO_TOKEN)
